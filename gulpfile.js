2e1662590ba82213d337bf8b3bbbfa205036e1ff
tmp42 <tmp42@mail.ru>
2021-04-25 00:32:47 +0300

    добавление изображений

--- gulpfile.js
+++ gulpfile.js
@@ -146,7 +146,7 @@ const reload = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/scss/**/*.scss", gulp.series(styles));
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
   gulp.watch("source/js/script.js", gulp.series(scripts));
   gulp.watch("source/*.html", gulp.series(html, reload));
 }
